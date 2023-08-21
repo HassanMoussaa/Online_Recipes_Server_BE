4a1f6a8169eaac98e1cfbc7db404ecf9a12c50ed
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-21 11:15:28 +0300

    added first files for backend server

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\FavoritesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+
+Route::middleware('auth:api')->group(function () {
+
+});
